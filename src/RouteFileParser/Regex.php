322a23e8a6ca10c8ff2cb2e03ba7b6aea6c24820
Ronnie Skansing <rskansing@gmail.com>
2015-03-22 12:24:32 +0100

    refactored

--- src/RouteFileParser/Regex.php
+++ src/RouteFileParser/Regex.php
@@ -16,17 +16,12 @@ class Regex implements RouteFileParser {
    * Parses the passed route file and return a specialized array
    * 
    * @param  string $file Absolute path to the route file
+   * @throws \Exception If $file is missing or empty
    * @return Array Lookup table for routes and a combined regex
    */
   public function digest($file)
   {
-    if(file_exists($file) === false) {
-      throw new \Exception('Route file not found.');
-    }
-    $routesData = require $file;
-    if(empty($routesData)) {
-      throw new \Exception('Route file is empty.');
-    }
+    $routesData = $this->getRoutesData($file);
     $routeRegexes = [];
     for($i = 0; $i < count($routesData) -1; ++$i) {
       $this->setRouteRegexData($routeRegexes, $routesData, $i);
@@ -41,6 +36,31 @@ class Regex implements RouteFileParser {
     return $routeRegexes;
   }
 
+  /**
+   * Gets the routes data and returns it
+   *
+   * @param $file
+   * @throws \Exception If $file is missing or empty
+   */
+  private function getRoutesData($file) {
+    if(file_exists($file) === false) {
+      throw new \Exception('Route file not found.');
+    }
+    $routesData = require $file;
+    if(empty($routesData)) {
+      throw new \Exception('Route file is empty.');
+    }
+
+    return $routesData;
+  }
+
+  /**
+   * Sets the group regex string with the constant REGEX_PREFIX between each route
+   * 
+   * @param array &$routeRegexes
+   * @param array &$routesData
+   * @param int $index
+   */
   private function setRouteRegexData(&$routeRegexes, &$routesData, $index) {
      if(isset($routeRegexes[$routesData[$index][self::VERB]]) === false) {
         $routeRegexes[$routesData[$index][self::VERB]] = self::REGEX_PREFIX;
